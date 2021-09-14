9e89a397ffa5bbcfeef8aee04d5b8eca73037247
ira <ira@ira>
2021-09-13 22:17:18 -0500

    update listener

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -9,10 +9,14 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +27,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
 
+    private lateinit var buttonDownload : Button
+    private lateinit var radioGroup: RadioGroup
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,35 +37,60 @@ class MainActivity : AppCompatActivity() {
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
-        custom_button.setOnClickListener {
+        /*custom_button.setOnClickListener {
             download()
+        }*/
+
+        buttonDownload = findViewById(R.id.button)
+        radioGroup = findViewById(R.id.radioGroup)
+        buttonDownload.setOnClickListener {
+            when (radioGroup.checkedRadioButtonId) {
+                R.id.radio_button_glide -> download(URL_GLIDE)
+
+                R.id.radio_button_repository -> download(URL_REPOSITORY)
+
+                R.id.radio_button_retrofit -> download(URL_RETROFIT)
+
+                else -> toast()
+
+            }
         }
     }
 
-    private val receiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context?, intent: Intent?) {
-            val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+        private val receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            }
         }
-    }
 
-    private fun download() {
-        val request =
-            DownloadManager.Request(Uri.parse(URL))
-                .setTitle(getString(R.string.app_name))
-                .setDescription(getString(R.string.app_description))
-                .setRequiresCharging(false)
-                .setAllowedOverMetered(true)
-                .setAllowedOverRoaming(true)
+        private fun download(url: String) {
+            val request =
+                    DownloadManager.Request(Uri.parse(url))
+                            .setTitle(getString(R.string.app_name))
+                            .setDescription(getString(R.string.app_description))
+                            .setRequiresCharging(false)
+                            .setAllowedOverMetered(true)
+                            .setAllowedOverRoaming(true)
 
-        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
-    }
+            val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+            downloadID =
+                    downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+        }
 
     companion object {
-        private const val URL =
+        private const val URL_GLIDE =
+            "https://github.com/bumptech/glide/archive/master.zip"
+        private const val URL_REPOSITORY =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val URL_RETROFIT =
+            "https://github.com/square/retrofit/archive/master.zip"
+
         private const val CHANNEL_ID = "channelId"
     }
 
+
+
+    private fun toast(){
+        Toast.makeText(applicationContext, getString(R.string.toast_message), Toast.LENGTH_SHORT).show()
+    }
 }
